c5c2a1c56cd551c14a1796183d3d63eca6d2be70
killomsc <359468277@qq.com>
2021-04-01 21:43:56 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rpc-framework-learning
-学习造轮子
+> 学习造轮子，尝试写一个rpc框架
+- 服务发现采用nacos
+- 传输采用netty
+- 序列化协议采用kryo或者fst
+- 
---
+++ src/test/java/com/killomsc/rpcframeworklearning/RpcFrameworkLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killomsc.rpcframeworklearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcFrameworkLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
